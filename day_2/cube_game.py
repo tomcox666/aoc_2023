d0930239653a4e51061c83e6d5b39d82d216d08d
Tom Cox <tomcox666@gmail.com>
2023-12-02 13:21:08 +0000

    completed solution for part one

--- day_2/cube_game.py
+++ day_2/cube_game.py
@@ -8,31 +8,32 @@ def get_game_ID(line):
 
 def check_colour_limits(line):
 
-    colour_counts = {'blue': 0, 'red': 0, 'green': 0}
     colour_limits = {'blue': 14, 'red': 12, 'green': 13}
     games = re.split(r';\s*', line)
 
     for game in games:
-        match = re.search(r'(\d+)\s+(\w+)', game)
-        if match:
-            count, colour = int(match.group(1)), match.group(2)
-            colour_counts[colour] += count
-            if colour_counts[colour] > colour_limits[colour]:
-                print(f"Warning: {colour} count exceeds the limit in one of the games.")
+        colour_counts = {'blue': 0, 'red': 0, 'green': 0}
+        cubes = re.split(r',\s*', game)
+        for cube in cubes:
+            match = re.search(r'(\d+)\s+(\w+)', cube)
+            if match:
+                count, colour = int(match.group(1)), match.group(2)
+                colour_counts[colour] += count
+                if colour_counts[colour] > colour_limits[colour]:
+                    return False
 
-    return colour_counts
+    return True
 
 def main(file_path):
     sum_of_ids = 0
 
     with open(file_path, "r") as file:
-
         for line in file:
-
-            game_ID = get_game_ID(line)
-            sum_of_ids += game_ID
+            if check_colour_limits(line):
+                game_ID = get_game_ID(line)
+                sum_of_ids += game_ID
     
-    print("Sum of ID's: {}".format(sum_of_ids))
+    print("Sum of ID's for part one: {}".format(sum_of_ids))
 
 if __name__ == "__main__":
     infile_path = 'puzzle_input.txt'
